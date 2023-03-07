aa37f3302eb498b05d86a85adc7b5230ea5f3524
hideonbush3 <ppoii0961@gmail.com>
2023-03-07 16:50:46 +0900

    저장소 초기화, index 페이지 작성

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import Main from "./components/pages/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
