c48f015ac4feadcc9aef9bfe4403d959dd51e3ee
hideonbush3 <ppoii0961@naver.com>
2023-03-07 17:16:00 +0900

    join, login, list, myinfo 작성, css 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Main from "./components/pages/Main";
 
+
 function App() {
   return (
     <div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <img src="/img/rubber-duck.png" alt="메인이미지" />
+    </main>
+  );
+};
+
+export default Home;
